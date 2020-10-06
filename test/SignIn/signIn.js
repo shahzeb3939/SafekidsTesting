8fc167cbce398468d2268676bc1c4374c271288e
Shahzeb Khan <shahzeb3939@gmail.com>
2020-10-06 18:57:07 +0500

    initial commit after successfully running first test

---
+++ test/SignIn/signIn.js
@@ -0,0 +1,8 @@
+describe ('Safekids App Test', function () {
+    
+    it ('Should go to Safekids App Home Page', () => {
+        browser.url('http://safekids-frontend.herokuapp.com/');
+        browser.pause(3000);
+    })
+
+})
